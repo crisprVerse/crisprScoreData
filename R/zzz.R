de257c1635273368a3c27e2c41a78b8497387de4
fortinj2 <fortin.jeanphilippe@gene.com>
2022-06-14 16:21:28 -0700

    Fixed .onLoad when called b crisprDesign

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,27 @@
-.onLoad <- function(libname, pkgname){
-    fl <- system.file("extdata",
-                      "metadata.csv",
-                      package=pkgname)
-    titles <- utils::read.csv(fl,
-                              stringsAsFactors=FALSE)$Title
-    notReady <- c("crispria_dnase_human_K562_hg38.bigWig",
-                  "crispria_mnase_human_K562_hg38.bigWig",
-                  "crispria_faire_human_K562_hg38.bigWig")
-    titles <- setdiff(titles, notReady)
-    ExperimentHub::createHubAccessors(pkgname, titles)
+.onLoad <- function(libname, pkgname) {
+  objs <- read.csv(system.file("extdata", "metadata.csv",
+                                package="crisprScoreData"),
+                    stringsAsFactors=FALSE)$Title
+  if (!length(objs))
+    stop("no objects found")
+ 
+  ## Functions to load objects by name:
+  ns <- asNamespace(pkgname)
+  sapply(objs,
+         function(xx) {
+           func = function(metadata = FALSE) {
+             if (!isNamespaceLoaded("ExperimentHub"))
+               attachNamespace("ExperimentHub")
+             eh <- query(ExperimentHub(), "crisprScoreData")
+             ehid <- names(query(eh, xx))
+             if (!length(ehid))
+               stop(paste0("resource ", xx,
+                           "not found in ExperimentHub"))
+             if (metadata)
+               eh[ehid]
+             else eh[[ehid]]
+           }
+           assign(xx, func, envir=ns)
+           namespaceExport(ns, xx)
+         })
 }
